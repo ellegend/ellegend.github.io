a464e036af3892ca0336355aa59fbc03a19d5b77
alvaro <54690140+ellegend@users.noreply.github.com>
2023-10-11 19:50:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-const axios = require('axios');
+document.addEventListener('DOMContentLoaded', () => {
+    const scanButton = document.getElementById('scanButton');
+    const scanResult = document.getElementById('scanResult');
 
-const encodedParams = new URLSearchParams();
-encodedParams.set('imageBase64', '<REQUIRED>');
+    scanButton.addEventListener('click', async () => {
+        const url = 'https://mrz-scanner.p.rapidapi.com/ScanMRZ';
+        const options = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded',
+                'X-RapidAPI-Key': '6eabc9718fmshbafb842632c779ap171d6fjsn2de8470bcddf',
+                'X-RapidAPI-Host': 'mrz-scanner.p.rapidapi.com'
+            },
+            body: new URLSearchParams({
+                imageBase64: '<BASE64_IMAGE_DATA>' // Reemplaza con datos de imagen válidos
+            })
+        };
 
-const options = {
-  method: 'POST',
-  url: 'https://mrz-scanner.p.rapidapi.com/ScanMRZ',
-  headers: {
-    'content-type': 'application/x-www-form-urlencoded',
-    'X-RapidAPI-Key': '6eabc9718fmshbafb842632c779ap171d6fjsn2de8470bcddf',
-    'X-RapidAPI-Host': 'mrz-scanner.p.rapidapi.com'
-  },
-  data: encodedParams,
-};
-
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
+        try {
+            const response = await fetch(url, options);
+            const result = await response.text();
+            scanResult.innerText = result;
+        } catch (error) {
+            console.error(error);
+            scanResult.innerText = 'Error al escanear MRZ';
+        }
+    });
+});
