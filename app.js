5f978ca8e2650ac3aeb809d0e6278cabb29ab448
alvaro <54690140+ellegend@users.noreply.github.com>
2023-10-11 19:33:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,75 +1,18 @@
-// Reemplaza con la configuración de Firebase para tu proyecto
-const firebaseConfig = {
-    apiKey: "AIzaSyDt5SxVGtMFAabwGsGgPrMGMxgEqy8hugI",
-    authDomain: "genesistest-a7306.firebaseapp.com",
-    projectId: "genesistest-a7306",
-    storageBucket: "genesistest-a7306.appspot.com",
-    messagingSenderId: "157072279225",
-    appId: "1:157072279225:web:157a0a8895043c8e2afd68",
-    measurementId: "G-NMRK6EZ6WC"
-};
+document.addEventListener('DOMContentLoaded', async () => {
+    const scanButton = document.getElementById('scanButton');
+    const resultContainer = document.getElementById('resultContainer');
 
-// Inicializa Firebase
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
+    scanButton.addEventListener('click', async () => {
+        const url = 'https://qr-code-lookup-api.p.rapidapi.com/%7BPATH%7D';
+        const options = { method: 'GET' };
 
-// Configura el escáner de QR
-const scanButton = document.getElementById("scanButton");
-
-scanButton.addEventListener("click", () => {
-    if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
-        const scannerOptions = { video: true };
-        navigator.mediaDevices
-            .getUserMedia(scannerOptions)
-            .then(handleSuccess)
-            .catch(handleError);
-    } else {
-        alert('El escáner de QR no es compatible con este navegador.');
-    }
-});
-
-function handleSuccess(stream) {
-    const video = document.createElement('video');
-    document.body.appendChild(video);
-
-    video.srcObject = stream;
-    video.play();
-
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
-    canvas.style.display = 'none';
-    document.body.appendChild(canvas);
-
-    const scanInterval = setInterval(() => {
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-        context.drawImage(video, 0, 0, canvas.width, canvas.height);
-        
-        const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-        const code = jsQR(imageData.data, imageData.width, imageData.height);
-
-        if (code) {
-            clearInterval(scanInterval);
-            handleQRCode(code.data);
+        try {
+            const response = await fetch(url, options);
+            const result = await response.text();
+            resultContainer.innerText = result;
+        } catch (error) {
+            console.error(error);
+            resultContainer.innerText = 'Error al escanear QR';
         }
-    }, 100);
-}
-
-function handleError(error) {
-    console.error('Error al acceder a la cámara:', error);
-}
-
-function handleQRCode(qrData) {
-    // Realiza una petición a Firebase para validar el código QR
-    const qrRef = database.ref('qr_codes/' + qrData);
-
-    qrRef.once('value', (snapshot) => {
-        if (snapshot.exists()) {
-            alert('Código QR válido: ' + qrData);
-        } else {
-            alert('Código QR no válido: ' + qrData);
-        }
-        // Cierra la cámara después de escanear
-        document.body.innerHTML = '';
     });
-}
+});
