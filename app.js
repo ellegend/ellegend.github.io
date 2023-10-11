fe227777a3b10d800b06566650746b9c64eca449
alvaro <54690140+ellegend@users.noreply.github.com>
2023-10-11 19:38:00 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    const scanButton = document.getElementById('scanButton');
-    const resultContainer = document.getElementById('resultContainer');
+const url = 'https://qr-code-and-barcode-manager.p.rapidapi.com/scan?format=QR_CODE';
+const data = new FormData();
+data.append('file', 'entrada_1.png');
 
-    scanButton.addEventListener('click', async () => {
-        const url = 'https://qr-code-lookup-api.p.rapidapi.com/%7BPATH%7D';
-        const options = { method: 'GET' };
+const options = {
+	method: 'POST',
+	headers: {
+		'X-RapidAPI-Key': '6eabc9718fmshbafb842632c779ap171d6fjsn2de8470bcddf',
+		'X-RapidAPI-Host': 'qr-code-and-barcode-manager.p.rapidapi.com'
+	},
+	body: data
+};
 
-        try {
-            const response = await fetch(url, options);
-            const result = await response.text();
-            resultContainer.innerText = result;
-        } catch (error) {
-            console.error(error);
-            resultContainer.innerText = 'Error al escanear QR';
-        }
-    });
-});
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
